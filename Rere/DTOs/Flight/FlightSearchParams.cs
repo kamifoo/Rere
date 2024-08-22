f8ed1e2a185eef5be792fd6dbc69151d822d9615
Kami.Foo <kidfu@live.com>
2024-08-23 10:18:05 +1200

    fix: make search params all nullable

--- Rere/DTOs/Flight/FlightSearchParams.cs
+++ Rere/DTOs/Flight/FlightSearchParams.cs
@@ -2,12 +2,12 @@ namespace Rere.DTOs.Flight;
 
 public class FlightSearchParams
 {
-    public string Id { get; set; }
-    public string FlightNumber { get; set; }
-    public string Airline { get; set; }
-    public string DepartureAirport { get; set; }
-    public string ArrivalAirport { get; set; }
+    public string? Id { get; set; }
+    public string? FlightNumber { get; set; }
+    public string? Airline { get; set; }
+    public string? DepartureAirport { get; set; }
+    public string? ArrivalAirport { get; set; }
     public DateTime? DepartureTime { get; set; }
     public DateTime? ArrivalTime { get; set; }
-    public string Status { get; set; }
+    public string? Status { get; set; }
 }
